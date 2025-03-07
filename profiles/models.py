fcc24867db84be1ea3dd7566435d20f13d498257
yohannes2025 <johannes.m.tekle@gmail.com>
2025-03-07 21:25:41 +0300

    update

--- profiles/models.py
+++ profiles/models.py
@@ -1,12 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import user
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+
+import profiles
 
 
 class Profile(models.Model):
   owner = models.OneToOneField(User, on_delete=models.CASCADE)
   created_at = models.DateTimeField(auto_now_add=True)
-  updated_at models.DateTimeField(auto_now=True)
+  updated_at = models.DateTimeField(auto_now=True)
   name = models.CharField(max_length=255, blank=True)
   content = models.TextField(blank=True)
   image = models.ImageField(
@@ -18,5 +20,12 @@ class Profile(models.Model):
 
   def __str__(self):
     return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+  if created:
+    Profile.objects.create(owner=instance)
+
+post_save.connect(create_profile, sender=User)
 
 
