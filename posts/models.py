9d187af8c2ca1a0aec4429fc5c92a136b4db2dfb
yohannes2025 <johannes.m.tekle@gmail.com>
2025-03-08 16:36:06 +0300

    update

--- posts/models.py
+++ posts/models.py
@@ -2,18 +2,41 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-def get_default_user():
-    return User.objects.first().id  # Use the first user in the database
-
-
 class Post(models.Model):
-    owner = models.ForeignKey(
-        User, on_delete=models.CASCADE, default=get_default_user)  # Set default
-    title = models.CharField(max_length=255)
-    content = models.TextField()
+    """
+    Post model, related to 'owner', i.e. a User instance.
+    Default image set so that we can always reference image.url.
+    """
+    image_filter_choices = [
+        ('_1977', '1977'),
+        ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'),
+        ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'),
+        ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'),
+        ('normal', 'Normal'),
+        ('nashville', 'Nashville'),
+        ('rise', 'Rise'),
+        ('toaster', 'Toaster'),
+        ('valencia', 'Valencia'),
+        ('walden', 'Walden'),
+        ('xpro2', 'X-pro II')
+    ]
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_rgq6aq', blank=True
+    )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
 
     def __str__(self):
-        return self.title
+        return f'{self.id} {self.title}'
