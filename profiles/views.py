c73bda1b287326dca2bb6b956b4dc32ddc142f82
yohannes2025 <johannes.m.tekle@gmail.com>
2025-03-08 14:05:33 +0300

    update

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -7,13 +6,11 @@ from .serializers import ProfileSerializer
 
 class ProfileList(APIView):
     """
-    API view to retrieve a list of all profiles.
+    List all profiles
+    No Create view (post method), as profile creation handled by django signals
     """
 
     def get(self, request):
-        """
-        Handles the GET request to retrieve all profiles.
-        """
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
