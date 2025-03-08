c016aae340871e0f2cd38a6afc3bc7e0accd22ce
yohannes2025 <johannes.m.tekle@gmail.com>
2025-03-08 14:29:55 +0300

    update

--- env.py
+++ env.py
@@ -1,2 +1,2 @@
 import os
-os.environ['CLOUDINARY_URL'] = cloudinary://<your_api_key >:<your_api_secret>@di2vmvljd
+os.environ['CLOUDINARY_URL'] = 'cloudinary://562429431716132:7kRg9xln7G9l3aZNBJU2Du7S7Gg@di2vmvljd'
