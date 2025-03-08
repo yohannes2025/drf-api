759c0dc434daa1f55a8dfdbef97842209de2d2a9
yohannes2025 <johannes.m.tekle@gmail.com>
2025-03-08 16:51:29 +0300

    update

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -44,11 +44,12 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',    
-    'django.contrib.staticfiles',
+    'django.contrib.messages',
     'cloudinary_storage',
+    'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
+
     'profiles',
     'posts',
 ]
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
-    path('posts/', include('posts.urls')),
-    path('', RedirectView.as_view(url='posts/')),
 ]
