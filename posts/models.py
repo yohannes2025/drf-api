52ba57e8ab5e3fef16598d2f80ab847338736f69
yohannes2025 <johannes.m.tekle@gmail.com>
2025-03-08 16:19:20 +0300

    update

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
-from profiles.models import Profile  # Ensure this is correct
+
+
+def get_default_user():
+    return User.objects.first().id  # Use the first user in the database
 
 
 class Post(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, default=get_default_user)  # Set default
     title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
