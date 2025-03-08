eb8d86d48dd10bcb8ee933b7c3976ca2a921bc1a
yohannes2025 <johannes.m.tekle@gmail.com>
2025-03-08 13:12:58 +0300

    update

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'cloudinary',
 
     'profiles',
--- env.py
+++ env.py
@@ -1,2 +1,2 @@
 import os
-os.environ['CLOUDINARY_URL'] = 'cloudinary'://
+os.environ['CLOUDINARY_URL'] = cloudinary://<your_api_key >:<your_api_secret>@di2vmvljd
